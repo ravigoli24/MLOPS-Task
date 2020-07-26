e02582e5383810a9dc43a5d48589b4eda07a44aa
ravigoli24 <63970192+ravigoli24@users.noreply.github.com>
2020-07-27 02:16:06 +0530

    Update CNN_Model.py

--- CNN_Model.py
+++ CNN_Model.py
@@ -47,12 +47,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        'root/mlops/cnn_dataset/training_set/',
+        '/mlops/cnn_dataset/training_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        'root/mlops/cnn_dataset/test_set/',
+        '/mlops/cnn_dataset/test_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
