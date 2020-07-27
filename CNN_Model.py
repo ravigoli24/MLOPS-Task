cefe547f09b1b93320e0ff7ac23fab1eccaebfcf
ravigoli24 <63970192+ravigoli24@users.noreply.github.com>
2020-07-28 03:53:40 +0530

    Update CNN_Model.py

--- CNN_Model.py
+++ CNN_Model.py
@@ -17,13 +17,14 @@ model = Sequential()
 model.add(Convolution2D(filters=32, 
                         kernel_size=(3,3), 
                         activation='relu',
-                   input_shape=(64, 64, 3)
+                  input_shape=(64, 64, 3)
                        ))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Convolution2D(filters=32, 
                         kernel_size=(3,3), 
                         activation='relu',
+                  input_shape=(64, 64, 3)
                        ))
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
