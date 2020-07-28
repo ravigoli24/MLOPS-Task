e81d22b296e01e990a9bd0c57631bfda3e913f06
ravigoli24 <63970192+ravigoli24@users.noreply.github.com>
2020-07-28 15:58:48 +0530

    Update CNN_Model.py

--- CNN_Model.py
+++ CNN_Model.py
@@ -23,8 +23,7 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Convolution2D(filters=32, 
                         kernel_size=(3,3), 
-                        activation='relu',
-                  input_shape=(64, 64, 3)
+                        activation='relu'
                        ))
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -38,7 +37,7 @@ model.add(Dense(units=1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 
-from keras.preprocessing.image import ImageDataGenerator
+from keras_preprocessing.image import ImageDataGenerator
 
 train_datagen = ImageDataGenerator(
         rescale=1./255,
@@ -62,7 +61,6 @@ model.fit(
         epochs=1,
         validation_data=test_set,
         validation_steps=800)
-model.save_weights('cat_dag.h5')
 
 score = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
