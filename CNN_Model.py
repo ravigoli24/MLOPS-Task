faa6c7805fde12339c5828137e85e9c9ac1b1868
Ravindra Goli <63970192+ravigoli24@users.noreply.github.com>
2020-08-06 22:22:01 +0530

    Update CNN_Model.py

--- CNN_Model.py
+++ CNN_Model.py
@@ -58,7 +58,7 @@ test_set = test_datagen.flow_from_directory(
         class_mode='binary')
 model.fit_generator(
         training_set,
-        steps_per_epoch=8000,
+        steps_per_epoch=800,
         epochs=1,
         validation_data=test_set,
         validation_steps=800)
